63839fb561bef549fbf615ee5c0bc931c3080ab1
AmirF <faizov.amir@list.ru>
2022-06-30 09:29:49 +0300

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,32 +9,11 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-        private var name: EditText?=null
-    private var growth: EditText?=null
-    private var weight: EditText?=null
-    private var age: EditText?=null
-    private var text_button:Button?=null
-    private var result:TextView?=null
-    var formula:Double = 2.7;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        name = findViewById(R.id.editText2)
-        result = findViewById(R.id.TextView)
-        growth = findViewById(R.id.editText3)
-        weight = findViewById(R.id.editText4)
-        age = findViewById(R.id.editText)
-        text_button = findViewById(R.id.button)
-        text_button?.setOnClickListener{
-            if(name?.text?.toString()?.equals("")==true || 0 > weight?.text.toString().toDouble() ||
-                weight?.text.toString().toDouble() > 250 ||0 > age?.text.toString().toDouble() ||
-                age?.text.toString().toDouble() > 150 || 0 > growth?.text.toString().toDouble() || growth?.text.toString().toDouble() > 250
-            ){
-                Toast.makeText(this,"Данные введены некорректно.",Toast.LENGTH_LONG).show()}
-            else{
-                formula = weight?.text.toString().toDouble()+age?.text.toString().toDouble()+growth?.text.toString().toDouble()+name?.text.toString().hashCode()
-                result?.text = "Ответ" + " " + formula.toString()
-            }
+
     }
-    }}
+}
